5da0f16e29b67f833ad8c48ba9d329c0d648df46
emalderete <nicolas.alderete95@live.com>
2022-03-11 22:17:36 -0300

    Agregada animación sobre nombre en navbar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 let navbar = document.querySelector("#navbar");
 let phantomContainer = document.querySelector(".phantom-container");
+let brand = document.querySelector(".navigation-brand");
+let miniName = document.querySelector(".mini-name");
+let headerSmall = document.querySelector(".header-container-s");
+let codeLogo = document.querySelector(".code-minilogo");
 
-window.addEventListener('scroll', () => {
+// Evento escuchador del scroll
+window.addEventListener('scroll', scrollingNavbar);
+
+// Definiciones de funciones:
+// La función scrollingNavbar sirve para indicarle al navbar del documento como debe comportarse de acuerdo al scrolling del mouse.
+function scrollingNavbar() {
     navbar.classList.toggle('fixed-top', window.scrollY > 290);
     if (window.scrollY > 290) {
         phantomContainer.classList.remove('d-none');
+        brand.classList.add('opacity');
+        miniName.classList.remove('effect-spacing-letter');
     } else {
         phantomContainer.classList.add('d-none');
+        brand.classList.remove('opacity');
+        miniName.classList.add('effect-spacing-letter');
     }
-});
+}
+
+// Esta función improvisada sirve unicamente para mostrar en consola las coordenadas sobre el eje Y del scroll del sitio.
+window.onscroll = () => {
+    let scrollY = window.scrollY;
+    console.log(scrollY);
+};
