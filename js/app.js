6734b0dbf198eac191717af60d42928d03e35c8e
emalderete <nicolas.alderete95@live.com>
2022-03-13 21:01:22 -0300

    Se agrega respuesta de scroll
    Se codifica la respuesta del scroll para los botones del navbar. También se retocan formularios.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let brand = document.querySelector(".navigation-brand");
 let miniName = document.querySelector(".mini-name");
 let headerSmall = document.querySelector(".header-container-s");
 let codeLogo = document.querySelector(".code-minilogo");
+let home = document.querySelector(".item-home");
+let portfolio = document.querySelector(".item-port");
+let skills = document.querySelector(".item-skill");
+let about = document.querySelector(".item-about");
+let contact = document.querySelector(".item-contact");
+
 
 // Evento escuchador del scroll
 window.addEventListener('scroll', scrollingNavbar);
@@ -12,6 +18,10 @@ window.addEventListener('scroll', scrollingNavbar);
 // La función scrollingNavbar sirve para indicarle al navbar del documento como debe comportarse de acuerdo al scrolling del mouse.
 function scrollingNavbar() {
     navbar.classList.toggle('fixed-top', window.scrollY > 290);
+    home.classList.toggle('active', window.scrollY >= 0 && window.scrollY < 648);
+    portfolio.classList.toggle('active', window.scrollY >= 648 && window.scrollY < 1384);
+    skills.classList.toggle('active', window.scrollY >= 1384);
+    // about.classList.toggle('active', window.scrollY >= 0);  ESTABAS INTENTANDO QUE EL SCROLL COLOQUE 'ACTIVE' EN LOS BOTONES DE LA PAGINA ABOUT
     if (window.scrollY > 290) {
         phantomContainer.classList.remove('d-none');
         brand.classList.add('opacity');
@@ -24,7 +34,7 @@ function scrollingNavbar() {
 }
 
 // Esta función improvisada sirve unicamente para mostrar en consola las coordenadas sobre el eje Y del scroll del sitio.
-// window.onscroll = () => {
-//     let scrollY = window.scrollY;
-//     console.log(scrollY);
-// };
+window.onscroll = () => {
+    let scrollY = window.scrollY;
+    console.log(scrollY);
+};
